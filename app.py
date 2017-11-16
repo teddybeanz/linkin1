a63055f9db77f47b5a25f50d99bdbd13253f6b43
teddybeanz <33247556+teddybeanz@users.noreply.github.com>
2017-11-16 11:12:47 +0800

    testing weather webhook
    testing weather webhook.

--- app.py
+++ app.py
@@ -1,19 +1,3 @@
-# -*- coding:utf8 -*-
-# !/usr/bin/env python
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
@@ -24,8 +8,6 @@ from urllib.error import HTTPError
 
 import json
 import os
-import webbrowser
-
 
 from flask import Flask
 from flask import request
@@ -42,72 +24,66 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processingRequest(req)
+    res = processRequest(req)
 
     res = json.dumps(res, indent=4)
     # print(res)
-	
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
 
-def processingRequest(req):
-    if req.get("result").get("action") != "linking1":
-        return{}
-    webbrowser.open('http://google.com')
-#    baseurl="https://google.com/"
-#    yql_query = makeYqlQuery(req)
-#    if yql_query is None:
-#        return{}
-#    yql_url = baseurl + urlencode({'q':yql_query}) + "&format=json"
-#    result = urlopen(yql_url).read()
+def processRequest(req):
+    if req.get("result").get("action") != "yahooWeatherForecast":
+        return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
     data = json.loads(result)
-    res=makeWebhookResult(data)
+    res = makeWebhookResult(data)
     return res
-	
 
-#def makeYqlQuery(req):
- #   result = req.get("result")
-  #  parameters = result.get("parameters")
-   # city = parameters.get("date")
-    #if city is None:
-     #   return None
 
-    #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
-    # query = data.get('query')
-    # if query is None:
-        # return {}
+    query = data.get('query')
+    if query is None:
+        return {}
 
-    # result = query.get('results')
-    # if result is None:
-        # return {}
+    result = query.get('results')
+    if result is None:
+        return {}
 
-    # channel = result.get('channel')
-    # if channel is None:
-        # return {}
+    channel = result.get('channel')
+    if channel is None:
+        return {}
 
-    # item = channel.get('item')
-    # location = channel.get('location')
-    # units = channel.get('units')
-    # if (location is None) or (item is None) or (units is None):
-        # return {}
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
 
-    # condition = item.get('condition')
-    # if condition is None:
-        # return {}
+    condition = item.get('condition')
+    if condition is None:
+        return {}
 
-	
-	
     # print(json.dumps(item, indent=4))
-    webbrowser.open('http://google.com')
-    thedate = data['result']['parameters']['date']
 
-	
-    speech = "Hey no problem okay I'll look for flights on" + thedate 
+    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
@@ -117,7 +93,7 @@ def makeWebhookResult(data):
         "displayText": speech,
         # "data": data,
         # "contextOut": [],
-        "source": "linking1"
+        "source": "apiai-weather-webhook-sample"
     }
 
 
